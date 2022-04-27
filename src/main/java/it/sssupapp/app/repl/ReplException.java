3a29255c3a5eb738b855644c95aa15708ffc3da6
Tredici <marcolucio1999@gmail.com>
2022-04-27 08:39:59 +0200

    Reorganize code

--- src/main/java/it/sssupapp/app/repl/ReplException.java
+++
@@ -1,8 +0,0 @@
-package it.sssupapp.app.repl;
-
-import java.lang.Exception;
-
-public class ReplException extends Exception {
-    public ReplException() { super(); }
-    public ReplException(String msg) { super(msg); }
-}
